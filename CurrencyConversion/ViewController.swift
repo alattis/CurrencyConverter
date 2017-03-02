be79e70481b6a9e5845500a2de5fbc74c1d8a5ef
andrew lattis <andrew@andrewlattis.com>
2017-03-02 14:35:48 -0600

    Initial Commit

---
+++ CurrencyConversion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyConversion
+//
+//  Created by andrew lattis on 3/2/17.
+//  Copyright © 2017 andrew lattis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
